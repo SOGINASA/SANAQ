13999e633213f2378a70a5c811eabededec78e36
Soginasa <artempinigin2008@gmail.com>
2026-08-24 23:44:30 +0500

    deploy

--- frontend/src/app/AppProviders.jsx
+++ frontend/src/app/AppProviders.jsx
@@ -15,7 +15,7 @@ function AccessibilitySync({ children }) {
   return children;
 }
 
-// qwefqwerцйуайцуefйцвцуацуwefwewef
+// qwefqwerцйуайцуefйцвцуацуwefwewefss
 
 function AuthSync({ children }) {
   const hydrate = useAuthStore((state) => state.hydrate);
